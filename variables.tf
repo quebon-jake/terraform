3ff67189a03526c7250753c5b19302bdfb7dd64d
박상현 <sanghyun@bagsanghyeon-ui-MacBookPro.local>
2023-11-08 17:04:54 +0900

    feat : test

--- variables.tf
+++ variables.tf
@@ -2,3 +2,12 @@ variable "region" {
   type        = string
   description = "AWS region"
 }
+
+variable "access_key" {
+  description = "access"
+  
+}
+variable "secret_key" {
+  description = "secret"
+  
+}
