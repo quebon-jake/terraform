6ba443b25804d0d183975202dab3ec91c7be1f8c
박상현 <sanghyun@bagsanghyeon-ui-MacBookPro.local>
2023-11-08 17:11:45 +0900

    feat : test

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region = var.region
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_KET_ID
 }
 
 resource "aws_vpc" "main" {
--- variables.tf
+++ variables.tf
@@ -2,12 +2,3 @@ variable "region" {
   type        = string
   description = "AWS region"
 }
-
-variable "AWS_ACCESS_KEY_ID" {
-  description = "access"
-  
-}
-variable "AWS_SECRET_KET_ID" {
-  description = "secret"
-  
-}
